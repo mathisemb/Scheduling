a0d9837ce3fe85e0ecd16b7dea88575851b7d65c
Mathis Embit <mathis.embit@gmail.com>
2021-05-14 21:11:03 +0200

    Rendu final du projet

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-Le programme se lance en exécutant la commande make dans le dossier du projet.
-J'ai laissé les tests que j'ai fais dans le main.
+**Pour lancer sur eclipse (avec e(fx)clipse) :**
+- Importer le projet dans eclipse
+- Clic droit sur le projet -> build path -> configure build path -> java build path -> libraries -> ajouter JavaFX et JavaFX SDK si ce n’est pas encore fait
+- run -> run configurations -> Java Application -> MyClass (càd la classe qui contient le main) -> Arguments -> ajouter dans VM arguments :
+
+```
+--module-path "chemin vers le dossier lib du dossier de l'installation de javafx" --add-modules javafx.controls,javafx.fxml
+-Djava.library.path=java
+```
+
+- puis cliquer sur Apply
+
+**Méthode choisirFichier :**
+
+Suivant l'OS sur lequel est lancé le programme, une erreur pourrait arriver. Lors du choix du fichier, dans la méthode choisirFichier du ControllerFenetre.java à la ligne 68. Il suffit alors de mettre le chemin absolu (selon les conventions de l'OS) du dossier que vous voulez ouvrir par défaut avec le FileChooser.
+
+PS : J'ai laissé mes tests dans le main et 2 instances tests (exemple2 et exemple3) dans le dossier data
--- java/application/ControllerFenetre.java
+++ java/application/ControllerFenetre.java
@@ -1,6 +1,10 @@
 package application;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.util.Random;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -10,7 +14,11 @@ import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Polygon;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
@@ -28,6 +36,7 @@ public class ControllerFenetre {
 	@FXML Label fichierChoisiFX;
 	@FXML Button btnQuitter;
 	@FXML Pane diagramme;
+	@FXML Label ms, wc, wf, wt;
 	
 	private String fichierChoisi;
 	
@@ -55,7 +64,7 @@ public class ControllerFenetre {
 	@FXML
     public void choisirFichier() throws Exception {
 		FileChooser fileChooser = new FileChooser();
-		fileChooser.setTitle("Exemple de FileChooser"); 
+		fileChooser.setTitle("FileChooser"); 
 		File homeDir = new File("./data");
 		fileChooser.setInitialDirectory(homeDir);
 		
@@ -72,7 +81,7 @@ public class ControllerFenetre {
 			}
 		}
 	}
-	
+	 
 	@FXML
     public void executer() throws Exception {
 		if (fichierChoisi != null) {
@@ -115,17 +124,117 @@ public class ControllerFenetre {
 				
 				// On peut maintenant appeller getSchedule
 				new MyClass().getSchedule(fichierChoisi, "data/res", structChoisie, ordreChoisi , backfilling);
+
+				File resFile = new File("data/res");
+			    FileReader fr = new FileReader(resFile);        
+			    BufferedReader br = new BufferedReader(fr);    
+			    String line;
+			    
+			    long makespan = 0;
+			    long sumWjCj = 0;
+			    long sumWjFj = 0;
+			    long sumWjTj = 0;
+		
+			    while((line = br.readLine()) != null) {
+			    	String[] tokens = line.split("\t");
+			    	// String id = tokens[0];
+			    	int p = Integer.parseInt(tokens[1]);
+			    	int s = Integer.parseInt(tokens[5]);
+
+			    	makespan = s + p; // on le met à jour à chaque boucle pour qu'en sortant on ait bien le dernier
+
+			    }
+			    fr.close();    			    
+			    
+			    // Construction du diagramme
+			    diagramme.getChildren().clear(); // on enlève l'ancien diagramme
+			    
+			    fr = new FileReader(resFile);        
+			    br = new BufferedReader(fr);    
+			    
+			    while((line = br.readLine()) != null) {
+			    	String[] tokens = line.split("\t");
+			    	String id = tokens[0];
+			    	int p = Integer.parseInt(tokens[1]);
+			    	int r = Integer.parseInt(tokens[2]);
+			    	int d = Integer.parseInt(tokens[3]);
+			    	int w = Integer.parseInt(tokens[4]);
+			    	int s = Integer.parseInt(tokens[5]);
+
+			    	int C = s + p;
+			    	int F = C - r;
+			    	int T = Math.max(0, C - d);
+			    	
+			    	sumWjCj += (w*C);
+			    	sumWjFj += (w*F);
+			    	sumWjTj += (w*T);
+			    	
+			    	int largeur = (int)(((double)p/makespan)*750);
+			    	int debut = (int)(((double)s/makespan)*750);
+			    	int milieu = (int)( ( (double)(s+( (double)p /2) ) /makespan )*750 );
+			    	
+			    	// Couleur de la task
+			    	Random rand = new Random();
+			    	int valeur = 1 + rand.nextInt(5 - 1);
+			    	Paint couleur;
+			    	switch(valeur) {
+			    		case 1 : couleur = Color.web("#A4036F"); // violet
+			    			break;
+			    		case 2 : couleur = Color.web("#048BA8"); // bleu
+			    			break;
+			    		case 3 : couleur = Color.web("#16DB93"); // vert
+			    			break;
+			    		case 4 : couleur = Color.web("#EFEA5A"); // jaune
+			    			break;
+			    		case 5 : couleur = Color.web("#F29E4C"); // orange
+			    			break;
+			    		default : couleur = Color.web("#F29E4C"); // orange
+			    			break;
+			    	}
+
+					Rectangle taskRectangle = new Rectangle(debut,30, largeur, 50); // (<->à gauche, <->au dessus, largeur, hauteur)
+					taskRectangle.setFill(couleur);
+					taskRectangle.setStroke(Color.BLACK);
+					Text taskId = new Text(id);
+					taskId.setLayoutX(milieu-7);
+					taskId.setLayoutY(60);
+					
+					Text start = new Text(String.valueOf(s));
+					start.setFont(new Font(10));
+					start.setLayoutX((int)((double)s/makespan * 750) - 5);
+					start.setLayoutY(100);
 				
-				// Calcul du makespan et des sommes
+					diagramme.getChildren().addAll(taskRectangle, taskId, start);
+
+			    }
+			    
+			    fr.close();
+			    
+			    ms.setText("Makespan = " + makespan);
+			    wc.setText("Sum Wj*Cj = " + sumWjCj);
+			    wf.setText("Sum Wj*Fj = " + sumWjFj);
+			    wt.setText("Sum Wj*Tj = " + sumWjTj);
+			    
+			    Line trait = new Line(0, 80, 760, 80);
+				trait.setStrokeWidth(3);
+				Polygon fleche = new Polygon(760, 75, 760, 85, 770, 80);
 				
+				if (makespan < 200) {
+					for (int i = 0 ; i <= makespan ; i++) {
+						Line graduation = new Line((int)((double)i/makespan * 750), 80, (int)((double)i/makespan * 750), 86);
+						graduation.setStrokeWidth(2);
+						
+						diagramme.getChildren().addAll(graduation);
+					}
+				}
 				
-				// Construction du diagramme
-				Rectangle taskRectangle = new Rectangle(0,50, 200, 50); // (<->à gauche, <->au dessus, largeur, hauteur)
-				taskRectangle.setFill(Color.CORNFLOWERBLUE);
-				Text taskId = new Text("01");
-				taskId.setLayoutX(80);
-				taskId.setLayoutY(80);
-				diagramme.getChildren().addAll(taskRectangle, taskId);
+				Text makespanText = new Text(String.valueOf(makespan));
+				makespanText.setFont(new Font(10));
+				makespanText.setLayoutX(745);
+				makespanText.setLayoutY(100);
+				
+				diagramme.getChildren().addAll(trait, fleche, makespanText);
+			    
 				
 			}
 			else {
